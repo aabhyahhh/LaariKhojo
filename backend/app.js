87d00c0e1fdc947225100ff06c4ef8a946d395d6
aabhyahhh <abhayatrivedi2005@gmail.com>
2025-06-18 11:42:18 +0530

    backend dist issue

--- backend/app.js
+++ backend/app.js
@@ -90,11 +90,5 @@ app.get("/api/all-users", async (req, res) => {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
 
 module.exports = app;
