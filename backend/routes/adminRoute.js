10b18dd0f3cb3edd279007356bbfdd4b715d1b31
aabhyahhh <abhayatrivedi2005@gmail.com>
2025-07-17 16:58:27 +0530

    build logs

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -2,29 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Import the controller and validator
-const { addPermission } = require('../controllers/admin/permissionController');
-const { getPermissions } = require('../controllers/admin/permissionController');
-const { deletePermission } = require('../controllers/admin/permissionController');
-const { updatePermission } = require('../controllers/admin/permissionController');
 const vendorImageController = require('../controllers/admin/vendorImageController');
-
-const { Validator, deleteValidator, updateValidator } = require('../helpers/adminValidator');
 
 const auth = require('../middlewares/authMiddleware');
 const isAdmin = require('../middlewares/isAdmin');
 
-// Add Permission by Admin
-router.post('/add-permission', auth, isAdmin, Validator, addPermission);
-
-//Get Permissions from Admin
-router.get('/get-permissions', auth, isAdmin, getPermissions);
-
-//Delete Permission by Admin
-router.post('/delete-permission', auth, isAdmin, deleteValidator, deletePermission);
-
-//Update Permission by Admin
-router.post('/update-permission', auth, isAdmin, updateValidator, updatePermission);
-
+// Only keep vendor image routes below
 // Upload carousel image for vendor
 router.post('/upload-vendor-image', auth, isAdmin, vendorImageController.uploadVendorImage);
 // Upload or update display picture for vendor
